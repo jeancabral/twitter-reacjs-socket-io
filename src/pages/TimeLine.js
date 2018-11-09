c4b22a717d66dbd0404e4bac3386559b9c102852
Jean Cabral <jeancabral@gmail.com>
2018-11-09 00:16:42 -0300

    WIP - Timeline

--- src/pages/TimeLine.js
+++ src/pages/TimeLine.js
@@ -6,10 +6,9 @@ import "./Timeline.css";
 import api from '../services/api';
 
 export default class TimeLine extends Component {
-
-
   state = {
     newTweet: '',
+    tweets: [],
   };
 
   handleInputChange = e => {
@@ -17,34 +16,44 @@ export default class TimeLine extends Component {
   }
 
   handleNewTweet = async e => {
-
     if (e.keyCode !== 13) return;
 
     const content = this.state.newTweet;
     const author = localStorage.getItem('username')
 
-    console.log(content, author)
-
     await api.post('/tweets', { author, content });
 
-    this.setState({ newTweet: ''})
+    this.setState({ newTweet: '' })
+  }
 
+  async componentDidMount() {
+    const response = await api.get('/tweets');
+    this.setState({ tweets: response.data })
   }
 
   render() {
+    console.log(this.state.tweets)
+
+    const { tweets } = this.state;
 
     return (
       <div className="timeline-wrapper">
-          <img height={24} src={twiiterLogo} alt="Twitter Clone" />
-          <form>
-            <textarea 
-              value={this.state.newTweet}
-              onChange={this.handleInputChange}
-              onKeyDown={this.handleNewTweet}
-              placeholder="O que está acontecendo?"
-            ></textarea>
-          </form>
+        <img height={24} src={twiiterLogo} alt="Twitter Clone" />
+        <form>
+          <textarea
+            value={this.state.newTweet}
+            onChange={this.handleInputChange}
+            onKeyDown={this.handleNewTweet}
+            placeholder="O que está acontecendo?"
+          ></textarea>
+        </form>
+        <ul className="tweet-list">
+          {tweets.map((tweet) =>
+            <li key={tweet._id}>{tweet.author}</li>
+          )}
+        </ul>
       </div>
     );
   }
 }
+
